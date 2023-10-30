e6d2f95f93c824ba37d93315a00da95620350f1a
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2023-10-30 20:59:00 +0900

    Delete EXE_wbc.bat

--- EXE_wbc.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-cd commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\wbc.ps1
-@echo off
-PowerShell -NoExit -Nologo
