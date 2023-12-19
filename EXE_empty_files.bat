ad8e7b6cb34f9afd0c7b60094892066d4ede4ff2
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2023-12-19 22:38:28 +0900

    Delete EXE_empty_files.bat

--- EXE_empty_files.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-cd commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\tools\ef.ps1
-@echo off
-PowerShell -NoExit -Nologo
