5afd40eacaaac36fdb6ea06d8571e39aafa25396
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2023-12-19 22:38:18 +0900

    Delete EXE_deploy.bat

--- EXE_deploy.bat
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\commands\tools\deploy.ps1
-@echo off
-PowerShell -NoExit -Nologo
