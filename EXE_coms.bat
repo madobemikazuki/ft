c10145302a1c16d6f41485835ca75fb63208ac52
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2023-12-21 20:13:39 +0900

    Add files via upload

---
+++ EXE_coms.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force 
+@echo off
+PowerShell clear
+PowerShell -Windowstyle Hidden -NoProfile -File .\commands\tools\coms.ps1
+@echo off
+PowerShell -Nologo
