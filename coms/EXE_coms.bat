bdd2a4ce8de6eaa1d2ea1cce401bdc289278de00
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-01-08 21:11:57 +0900

    Delete coms directory

--- coms/EXE_coms.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-cd .\commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force 
-@echo off
-PowerShell clear
-PowerShell -Windowstyle Hidden -NoProfile -File .\tools\coms.ps1
-@echo off
-PowerShell -Nologo
--- coms/commands/ft_core/io/read_registed_people_fromT.ps1
+++
@@ -1,2 +0,0 @@
-﻿# ファイル名をよく確認すること
-return Import-Csv -Path "${HOME}\Downloads\TEMP\登録者管理リスト.csv" -Encoding Default
