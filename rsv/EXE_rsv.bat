f2ce1e9c905bf010e3bad75deb2bdbb0ff079381
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-04-18 22:21:02 +0900

    Delete rsv directory

--- rsv/EXE_rsv.bat
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-cd .\commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force 
-@echo off
-PowerShell clear
-PowerShell -Windowstyle Hidden -NoProfile -File .\rsv.ps1
-
