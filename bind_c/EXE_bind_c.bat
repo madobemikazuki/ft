6ede41fb82b8112dc05154908cab74748feb31a1
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-05-13 23:00:35 +0900

    Delete bind_c directory

--- bind_c/EXE_bind_c.bat
+++
@@ -1,9 +0,0 @@
-﻿@echo off
-cd .\commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\bind_c.ps1
-@echo off
-PowerShell -NoExit -Nologo
-
