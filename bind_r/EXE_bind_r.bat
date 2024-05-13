63ec84655968834101eb46fe189187b38e53cc4d
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-05-13 23:00:49 +0900

    Delete bind_r directory

--- bind_r/EXE_bind_r.bat
+++
@@ -1,9 +0,0 @@
-﻿@echo off
-cd .\commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\bind_r.ps1
-@echo off
-PowerShell -NoExit -Nologo
-
