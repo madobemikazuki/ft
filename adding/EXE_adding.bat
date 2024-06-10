f11c517074d263eab14d90aa69b5741c0d8b881f
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-06-10 21:33:57 +0900

    Delete adding directory

--- adding/EXE_adding.bat
+++
@@ -1,9 +0,0 @@
-﻿@echo off
-cd .\commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\adding.ps1
-@echo off
-PowerShell -NoExit -Nologo
-
