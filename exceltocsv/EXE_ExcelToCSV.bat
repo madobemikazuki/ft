06d992ce2fbf5dc394966318f0a16afc4662827d
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-06-10 21:33:36 +0900

    Delete exceltocsv directory

--- exceltocsv/EXE_ExcelToCSV.bat
+++
@@ -1,9 +0,0 @@
-﻿@echo off
-cd .\commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\ExcelToCSV.ps1
-@echo off
-PowerShell -NoExit -Nologo
-
