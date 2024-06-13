5db9ee8af54d41fc6362004d8327c8d3c1183d42
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-06-13 20:58:35 +0900

    Add files via upload

---
+++ rsv/EXE_bind_c.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+cd .\commands
+PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
+@echo off
+PowerShell clear
+PowerShell . .\bind_c.ps1
+@echo off
+PowerShell -NoExit -Nologo
+
---
+++ rsv/EXE_bind_r.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+cd .\commands
+PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
+@echo off
+PowerShell clear
+PowerShell . .\bind_r.ps1
+@echo off
+PowerShell -NoExit -Nologo
+
