c56903121935093f15c5848d72c9c9000064304a
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-06-15 21:45:05 +0900

    Add files via upload

---
+++ gZEN/EXE_one_array.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+cd .\commands\tools
+PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
+@echo off
+PowerShell clear
+PowerShell . .\one_array.ps1
+@echo off
+PowerShell -NoExit -Nologo
+
