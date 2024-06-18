8fc305cd3189bf53e4105867b8bbdcc05454daf4
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-06-18 22:04:11 +0900

    Delete gZEN directory

--- gZEN/EXE_one_array.bat
+++
@@ -1,9 +0,0 @@
-﻿@echo off
-cd .\commands\tools
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\one_array.ps1
-@echo off
-PowerShell -NoExit -Nologo
-
