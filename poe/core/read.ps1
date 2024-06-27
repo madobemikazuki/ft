bac364cf3fccba49f61f4c620195eeea335c3732
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-06-27 22:44:40 +0900

    Add files via upload

---
+++ poe/core/read.ps1
@@ -0,0 +1,21 @@
+﻿Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+
+function fn_Read {
+  Param(
+    [Parameter(Mandatory = $True, Position = 0)]
+    [ValidatePattern("\.csv$|\.json$")]$_path
+  )
+  switch -Regex ($_path) {
+    "\.csv$" {
+      return Import-Csv -Path $_path -Encoding Default
+    }
+    "\.json$" {
+      return Get-Content -Path $_path -Encoding UTF8 | ConvertFrom-Json
+    }
+    Default {
+      Write-Host "拡張子が該当しないので終了。"
+      exit 0
+    }
+  }
+}
