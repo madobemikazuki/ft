55a41ab4927c9c9b8b6dcee24c25237f80de0130
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-08-26 20:41:12 +0900

    Add files via upload

---
+++ commands/wid/commands/utils/ToWide.ps1
@@ -0,0 +1,11 @@
+﻿Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+
+function ToWide {
+  Param(
+    [Parameter(Mandatory = $True)][String]$half_string
+  )
+  Add-Type -AssemblyName "Microsoft.VisualBasic"
+  [Microsoft.VisualBasic.Strings]::StrConv($half_string, [Microsoft.VisualBasic.VbStrConv]::Wide)
+}
+
