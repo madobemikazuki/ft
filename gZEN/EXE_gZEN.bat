f3c20b00556a376c525c8f705296a6ba67f96a64
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-11-05 22:13:13 +0900

    Delete gZEN directory

--- gZEN/EXE_gZEN.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd .\commands\ft_utils
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-PowerShell -command "(Measure-Command {. .\gZEN.ps1}).TotalSeconds"
-PowerShell -NoExit -Nologo
