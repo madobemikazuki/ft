651db4cff0b8398f8df6df45e21892dfd0a36c7f
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-11-12 23:06:37 +0900

    Delete applicants directory

--- applicants/EXE_applicants.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd .\commands\ft_utils
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-PowerShell -command "(Measure-Command {. .\applicants.ps1} ).TotalSeconds"
-PowerShell -NoExit -Nologo
