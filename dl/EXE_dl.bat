1b20bfb26ef0e513e0cb0856abe4e2e5eecdd46f
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-11-12 23:06:15 +0900

    Delete dl directory

--- dl/EXE_dl.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd .\commands\tools
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell  -command "(Measure-Command {. .\dl.ps1}).TotalSeconds"
-PowerShell -NoExit -Nologo
