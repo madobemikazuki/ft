5d4519a19d393ab0e494fa31d82ee1a9507a8401
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-11-17 23:03:12 +0900

    Delete FnD directory

--- FnD/EXE_FnD.Bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd .\commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force 
-PowerShell -command "(Measure-Command {. .\FnD.ps1}).TotalSeconds"
-PowerShell -NoExit -Nologo
