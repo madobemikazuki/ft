41371ef02a2ccf420e68e081741e6a869cf7379d
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-11-24 20:17:32 +0900

    Delete commands directory

--- commands/cnc/EXE_cnc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd .\commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell . .\cnc.ps1
-@echo off
-PowerShell -NoExit -Nologo
