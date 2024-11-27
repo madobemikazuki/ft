c3864c67ac00ca551b19b4822071d6db8f0f45ef
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-11-27 23:09:37 +0900

    Delete commands directory

--- commands/ed/EXE_ed.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd commands
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-PowerShell clear
-:input
-PowerShell -command "(Measure-Command {. .\ed.ps1}).TotalSeconds"
-goto :input
