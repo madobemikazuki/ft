c943d467d6d7d55156d0fc7451876eade065d137
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-12-09 23:32:20 +0900

    Delete commands directory

--- commands/wid/EXE_wid.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd .\commands\ft_utils
-PowerShell Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-@echo off
-PowerShell clear
-PowerShell -command "(Measure-Command {. .\wid.ps1}).TotalSeconds"
-PowerShell -NoExit -Nologo
