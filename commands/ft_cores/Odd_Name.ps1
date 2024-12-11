08072eca025a9a9ac9625defbbab9e5bda60c139
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2024-12-11 23:11:02 +0900

    Add files via upload

---
+++ commands/ft_cores/Odd_Name.ps1
@@ -0,0 +1,16 @@
+﻿class Odd_Name {
+<#
+書類の特定の項目に記載する文字列が規則通り転記するとと冗長すぎるときや、
+なぜか半角カタカナが用いられたりする意味不明なデータを相手にするとき、
+そのような汚い文字列の代わりとなるより適切な文字列をオブジェクトから参照するメソッドです。
+ $_odds として渡すオブジェクトにあらかじめJSONファイルなどで定義していけば、
+ソースコードを修正することなく汚い文字列に対処することが期待できます。
+#>
+
+  static [String]To_Appropriate([String]$_key, [PSCustomObject]$_odd_dict) {
+    $odd_keys = $_odd_dict.psobject.properties.Name
+    $result = if ($odd_keys.Contains($_key)) { $_odd_dict.$_key } else { $_key }
+    return $result
+  }
+}
+
