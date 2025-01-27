664c189a73c0ff3d3995275c5a7c201a949e6512
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2025-01-27 23:21:27 +0900

    Add files via upload

---
+++ commands/APD_Watcher/commands/ft_cores/FT_Path.ps1
@@ -0,0 +1,27 @@
+﻿class FT_Path {
+
+  static CopyToDL ([String]$_from_path, [String]$_destination_path) {
+    Write-Host "　From: " $_from_path -BackgroundColor DarkBlue
+    $private:file_name = Split-Path $_from_path -Leaf 
+    $private:dist_path = ($_destination_path + $file_name)  
+    Write-Host "　　To: " $dist_path -BackgroundColor DarkBlue
+    Copy-Item -Path $_from_path -Destination $dist_path -Force
+  }
+
+  static [String] Fixed_Path ([String]$_folder_path) {
+    $private:parsed_folder_path = switch ($_folder_path) {
+      #{ $_.Contains("＆") } { $_ -replace "＆", "$"; break; }
+      # 実行環境ではこのコードでは動かない？
+      { $_.Contains("&") -or $_.Contains("＆") } { $_ -replace "&|＆", "$"; break; }
+      
+      { $_.Contains("Downloads") } {
+        $local_home_folder_path = (Get-Variable | Where-Object { $_.Name -eq "HOME" }).Value; 
+        ($local_home_folder_path + $_);
+        break; 
+      }
+      default { $_ }
+    }
+    return $parsed_folder_path
+  }
+}
+
---
+++ commands/APD_Watcher/commands/tools/watchers/APD_watcher.ps1
@@ -0,0 +1,51 @@
+﻿Param(
+  [Parameter(Mandatory = $True, Position = 0)]
+  [ValidatePattern("^[0-9]{2}\-[0-9]{6}$")]
+  [String[]]$_central_nums
+)
+
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+
+. ..\..\ft_cores\FT_IO.ps1
+. ..\..\ft_cores\FT_Path.ps1
+. ..\..\ft_cores\FT_Message.ps1
+
+# 前回起動時のEventSubscriberを全て消す
+Get-EventSubscriber -Force | Unregister-Event -Force
+
+
+$private:command_name = Split-Path -Leaf $PSCommandPath
+[FT_Message]::execution($command_name)
+
+$script:APD_Config = [FT_IO]::Read_JSON_Object(".\config\csvAPD.json")
+
+
+$script:APD_watcher = New-Object System.IO.FileSystemWatcher
+$APD_watcher.Path = [FT_Path]::Fixed_Path($APD_Config.folder)
+$APD_watcher.Filter = $APD_Config.file_name
+$APD_watcher.IncludeSubdirectories = $false
+$APD_watcher.EnableRaisingEvents = $True
+
+$script:watcher_name = ("Created_" + $command_name)
+
+$action = {
+  Set-Location ..
+  . .\csvAPD.ps1 $Event.MessageData
+  Set-Location .\watchers
+}
+
+$reg_dict = @{
+  InputObject      = $APD_watcher
+  EventName        = "Created"
+  SourceIdentifier = $watcher_name
+  Action           = $action
+  MessageData      = $_central_nums
+}
+
+Register-ObjectEvent @reg_dict
+Write-Host $watcher_name" 起動"
+while ($true) {
+  Start-Sleep 1
+}
+
